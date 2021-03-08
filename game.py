12668bb332a8112f9966789bf6889bfbb63d7068
zhangy111 <henryzusa@gmail.com>
2021-03-07 21:19:21 -0800

    simultaneous player & opp L-in-a-row now results in a draw

--- game.py
+++ game.py
@@ -6,7 +6,8 @@ Created on Fri Mar  5 00:16:40 2021
 @author: henry
 
 """
-import Board
+from board import Board
+from search import SearchStrategy
 
 class Game:
     
@@ -16,9 +17,9 @@ class Game:
     player2_is_ai = False
     player1_color = "white"
     player2_color = "black"
-    I_m = None
+    b = None
     
-    def __init__(self, max_turns, p1_ai, p2_ai, board_size=4):
+    def __init__(self, max_turns=50, p1_ai=True, p2_ai=True, board_size=4):
         '''
         Set up the game
         Pre-condition: max_turns >= 0, board_size >0
@@ -27,65 +28,78 @@ class Game:
         self.max_turns = max_turns
         self.player1_is_ai = p1_ai
         self.player2_is_ai = p2_ai
-        self.I_m = Board(board_size)
+        self.b = Board(board_size)
         
         
     def check_win_loss(self, player_color):
         '''
         Game ending condition checks
-        Pre-condition: game board I_m where each board space is sorted s.t. 
-                        the 0th element of the stack is the largest of the stack
+        Pre-condition: game board b where each board space is sorted s.t. 
+                        the last element of the stack is the largest of the stack
         Post-condition: returns game state
         '''
         # this function uses indexing starting at 1 due to notation decisions
         if self.num_turns == self.max_turns:
             return 'draw'
         
-        isHoriz = [0, 0, 0, 0] # tracking counts for horizontal columns
-        isDiag = [0, 0] # [positive slope diagonal, negative slope diagonal]
+        I_m = self.b.board
+        win_flag = False
+        loss_flag = False
+        is_horiz = [0, 0, 0, 0] # tracking counts for horizontal columns
+        is_diag = [0, 0] # [positive slope diagonal, negative slope diagonal]
          
-        for j in range (1, self.I_m.size + 1): # j = [a, b, c, d] as described in notation
-             isVert = 0 # tracking number of pieces of a player's color in a vertical column
+        for j in range (1, self.b.size + 1): # j = [a, b, c, d] as described in notation
+             is_vert = 0 # tracking number of pieces of a player's color in a vertical column
              
-             for k in range (1, self.I_m.size + 1):
-                 if self.I_m[(j,k)] == None:
+             for k in range (1, self.b.size + 1):
+                 if len(I_m[(j,k)]) == 0:
                      continue
-                 topPiece = self.I_m[(j,k)][0] # only the top (visible) piece at $I_m[j][k]$ matters 
-                 if topPiece[0] == player_color:
-                     isVert += 1
-                     isHoriz[k] += 1
+                 top_piece = I_m[(j,k)][-1] # get the largest piece
+                 if top_piece[0] == player_color:
+                     is_vert += 1
+                     is_horiz[k-1] += 1
                  else:
-                     isVert -= 1
-                     isHoriz[k] -= 1
+                     is_vert -= 1
+                     is_horiz[k-1] -= 1
                      
                  if j == k: # check if we are in a negative slope diagonal space 
-                    if topPiece[0] == player_color:
-                         isDiag[1] += 1
+                    if top_piece[0] == player_color:
+                         is_diag[0] += 1
                     else:
-                         isDiag[1] -= 1
+                         is_diag[0] -= 1
                  if j + k == 5: # check if we are in a positive slope diagonal space 
-                    if topPiece[0] == player_color:
-                         isDiag[2] += 1
+                    if top_piece[0] == player_color:
+                         is_diag[1] += 1
                     else:
-                         isDiag[2] -= 1
+                         is_diag[1] -= 1
                          
-             if isVert == 4: # player 4-in-a-row 
-                 return 'win'
-             if isVert == -4: # opponent 4-in-a-row
-                 return 'loss'
+             if is_vert == 4: # player 4-in-a-row 
+                 win_flag = True
+             if is_vert == -4: # opponent 4-in-a-row
+                 loss_flag = True
              
-        for p in isHoriz: # tally up score for horizontal rows 
-            if isHoriz[p] == 4:
-                 return 'win'
-            if isHoriz[p] == -4:
-                 return 'loss'
+        for p in is_horiz: # tally up score for horizontal rows 
+            if is_horiz[p] == 4:
+                 win_flag = True
+            if is_horiz[p] == -4:
+                 loss_flag = True
 
-        for q in [1, 2]: # tally up score for diagonals
-            if isDiag[q] == 2:
-                 return 'win'
-            if isDiag[q] == -2:
-                 return 'loss'
+        for q in [0, 1]: # tally up score for diagonals
+            if is_diag[q] == 4:
+                 win_flag = True
+            if is_diag[q] == -4:
+                 loss_flag = True
 
+        # case when the move resulted in both player and opponent 4-in-a-row
+        if win_flag and loss_flag:
+            return 'draw'
+        
+        if win_flag:
+            return 'win'
+        
+        if loss_flag:
+            return 'loss'
+        
         return 'na'
     
     
@@ -118,15 +132,12 @@ class Game:
             print("Are you moving it from the starting stack? If so, enter 0 for the below.")
             old_position_row = input("Where is this piece from? Enter the row: ")
             old_position_col = input("Where is this piece from? Enter the column: ")
-            old_position = (int(old_position_row), int(old_position_col))
+            old_pos = (int(old_position_row), int(old_position_col))
             new_position_row = input("Where is this piece going? Enter the row: ")
             new_position_col = input("Where is this piece going? Enter the column: ")
-            new_position = (int(new_position_row), int(new_position_col))
+            new_pos = (int(new_position_row), int(new_position_col))
             print("You want to move", piece, old_position, new_position)
-            if old_position != (0,0):
-                self.I_m.place_piece(piece, new_position, old_position)
-            else:
-                self.I_m.place_piece(piece, new_position)
+            self.b.make_move(old_pos, new_pos)
                
     
     def run_game (self):
@@ -164,3 +175,21 @@ class Game:
         if condition == 'loss':
             return -1
         return 0
+    
+    
+# testing positive diagonal
+g = Game()
+b = g.b
+N = b.size + 1
+b.make_move((0,1), (1,1))
+b.make_move((N,1), (1,4))
+b.make_move((0,1), (2,2))
+b.make_move((N,1), (2,3))
+b.make_move((0,1), (3,3))
+b.make_move((N,1), (3,2))
+b.make_move((0,1), (4,4))
+assert(g.check_win_loss('white') == 'win')
+# testing negative diagonal
+b.make_move((N,1), (4,1))
+b.make_move((4,4), (4,3))
+assert(g.check_win_loss('black') == 'win')
