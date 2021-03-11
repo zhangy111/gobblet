f2a0735b9a5665ba8c87619e24ed283687b40af6
TJ Smith <tj@usdd.com>
2021-03-10 21:29:15 -0700

    Merged score files into score_jennifer

--- score_jennifer.py
+++ score_jennifer.py
@@ -17,6 +17,7 @@ Todo:
 from board import Board
 import random as rand
 from operator import add
+from renderer import Renderer
 
 class ScoringStrategy:
     all_strats = []
@@ -30,7 +31,7 @@ class ScoringStrategy:
             return self.all_strats
         return self.scoring_strat(*args, **kwargs)
 
-# @ScoringStrategy
+@ScoringStrategy
 def random_score(board, *args, **kwargs):
     """
     :param board: Board Object
@@ -140,6 +141,7 @@ def linarow_score(board, player_color, L=4, *args, **kwargs):
 # assert(linarow_score('black', b, 3) == 100)
 # assert(linarow_score('black', b, 2) == 100)
 
+@ScoringStrategy
 def agg_linarow_score(board, player_color, L=4, *args, **kwargs):
     agg_score = 0
     for i in range(L+1):
@@ -250,6 +252,7 @@ def consecutive_score(board, player_color, L=4, *args, **kwargs):
 # assert(consecutive_score('black', b, 3) == 200)
 # assert(consecutive_score('black', b, 2) == 300)
 
+@ScoringStrategy
 def agg_consec_score(board, player_color, *args, **kwargs):
     agg_score = 0
     for i in range(board.size+1):
@@ -260,10 +263,11 @@ def agg_consec_score(board, player_color, *args, **kwargs):
 def agg_linarowconsec_score(board, player_color, *args, **kwargs):
     return agg_consec_score(board, player_color) + agg_linarow_score(board, player_color)
 
+@ScoringStrategy
 def comb(board, player_color, *args, **kwargs):
     return agg_consec_score(board, player_color) + 2*agg_linarow_score(board, player_color)
 
-# @ScoringStrategy
+@ScoringStrategy
 def combo2(board, player_color, *args, **kwargs):
     return chess_like_score(board, player_color) + value_map_score(board, player_color)
 
@@ -287,6 +291,7 @@ def num_pieces(board, *args, **kwargs):
     return pieces
 
 import copy
+@ScoringStrategy
 def combo3(board, player_color, *args, **kwargs):
     """
     strategy calculates agg_consec and agg_linarow for all levels of the board
@@ -320,6 +325,7 @@ def isGameOver(board, *args, **kwargs):
     else:
         return -1
 
+@ScoringStrategy
 def freetomove_score(board, player_color, *args, **kwargs):
     """
     :param player_color: black/white
@@ -345,30 +351,179 @@ def freetomove_score(board, player_color, *args, **kwargs):
 # b.place_piece(('black', 4), (1,1))
 # assert(freetomove_score('black', b) == 400)
 
+@ScoringStrategy
 def combo4(board, player_color, *args, **kwargs):
     aggscore = agg_linarowconsec_score(board, player_color)
     aggscore += freetomove_score(board, player_color)
     return aggscore
 
+@ScoringStrategy
 def combo5(board, player_color, *args, **kwargs):
     return combo4(board, player_color) + combo_score(board, player_color)
 
+@ScoringStrategy
 def combo6(board, player_color, *args, **kwargs):
     return combo2(board, player_color) + combo4(board, player_color) + combo_score(board, player_color)
 
+@ScoringStrategy
 def combo7(board, player_color, *args, **kwargs):
     agg_score = chess_like_score(board, player_color) * agg_linarowconsec_score(board, player_color)
     agg_score += value_map_score(board, player_color) * agg_linarowconsec_score(board, player_color)
     return agg_score
 
+@ScoringStrategy
 def combo8(board, player_color, *args, **kwargs):
     aggscore = freetomove_score(board, player_color) * agg_linarowconsec_score(board, player_color)
     return combo7(board, player_color) + aggscore
 
+@ScoringStrategy
 def combo9(board, player_color, *args, **kwargs):
     aggscore = freetomove_score(board, player_color) * value_map_score(board, player_color)
     return combo7(board, player_color) + aggscore
 
+@ScoringStrategy
 def combo10(board, player_color, *args, **kwargs):
     aggscore = freetomove_score(board, player_color) * chess_like_score(board, player_color)
     return combo7(board, player_color) + aggscore
+
+
+################################################################################
+## Henry's part
+################################################################################
+
+
+@ScoringStrategy
+def get_all_scoring_functions():
+    return
+
+@ScoringStrategy
+def chess_like_score(board, player_color, piece_value=None, *args, **kwargs):
+    """
+    :param player_color: "white" or "black"
+    :param board: Board object
+    :param piece_value: Mapping of gobblets to value based on their sizes
+    :return: score (net)
+    """
+    I_m = board.board
+    score = 0
+    
+    if piece_value is None: # default value mapping
+        piece_value = {('white', 1): 1, ('white', 2): 3, ('white', 3): 5, 
+                       ('white', 4): 10, ('black', 1): 1, ('black', 2): 3,
+                       ('black', 3): 5, ('black', 4): 10}
+    
+    for j in range (1, board.size+1):
+        for k in range (1, board.size+1):
+            if I_m.get((j, k)) is not None:
+                pieces = I_m.get((j, k))
+                #print(pieces)
+                for piece in pieces:
+                    if piece[0] == player_color:
+                        score += piece_value[piece]
+                    else:
+                        score -= piece_value[piece]
+    return score
+
+@ScoringStrategy
+def value_map_score(board, player_color, piece_value=None, diag_weight=1, 
+                    *args, **kwargs):
+    """
+    :param player_color: "white" or "black"
+    :param board: Board object
+    :param piece_value: Mapping of gobblets to value based on their sizes
+    :param diag_weight: Additional scoring weight applied to diagonal spaces
+    :return: score (net)
+    """
+    I_m = board.board
+    score = 0
+    
+    # diag_spaces = set() 
+    # for i in range (1, board.size+1):
+    #     diag_spaces.add((i, i))
+    #     diag_spaces.add((i, board.size+1-i))
+    # print(diag_spaces)
+    
+    if piece_value is None: # default value mapping
+        piece_value = {('white', 1): 1, ('white', 2): 2, ('white', 3): 3, 
+                       ('white', 4): 4, ('black', 1): 1, ('black', 2): 2,
+                       ('black', 3): 3, ('black', 4): 4}
+    
+    for j in range (1, board.size+1):
+        for k in range (1, board.size+1):    
+            
+            is_diag = 0
+            if j == k or k == board.size + 1 - j:
+                is_diag = 1
+                
+            if len(I_m.get((j, k))) > 0:
+                pieces = I_m.get((j, k))
+                #print(pieces)
+                
+                for piece in pieces:
+                    if piece[0] == player_color:
+                        score += piece_value[piece] * (1 + is_diag * diag_weight)
+                    else:
+                        score -= piece_value[piece] * (1 + is_diag * diag_weight)
+                        
+    return score
+
+
+@ScoringStrategy
+def combo_score(board, player_color, L=4, piece_value=None, diag_weight=1, 
+                    score_weight=None, *args, **kwargs):
+    """
+    :param player_color: "white" or "black"
+    :param board: Board object
+    :param L: L-in-a-row (must be less than board size)
+    :param piece_value: Mapping of gobblets to value based on their sizes
+    :param diag_weight: Additional scoring weight applied to diagonal spaces
+    :return: score
+    """
+    score = 0
+    
+    if score_weight is None:
+        score_weight = {'random': 1, 'linarow': 1, 'consecutive': 1, 
+                         'freetomove': 1, 'chesslike': 1, 'valuemap': 1}
+
+    score += random_score(board) * score_weight['random']
+    score += linarow_score(board, player_color, L) * score_weight['linarow']
+    score += consecutive_score(board, player_color, L) * score_weight['consecutive']
+    # score += freetomove_score(board, player_color) * score_weight['freetomove']
+    score += chess_like_score(board, player_color) * score_weight['chesslike']
+    score += value_map_score(board, player_color) * score_weight['valuemap']
+    
+    return score
+
+
+if __name__ == "__main__":
+    # testing positive diagonal
+    b = Board(4)
+    N = b.size + 1
+    b.make_move((0,1), (1,1))
+    b.make_move((N,1), (1,2))
+    b.make_move((0,1), (2,2))
+    b.make_move((N,1), (2,4))
+    b.make_move((0,1), (3,3))
+    b.make_move((N,1), (3,4))
+    b.make_move((0,1), (4,4))
+
+    print(get_all_scoring_functions())
+
+    # assert(value_map_score(b, 'white') == 11)
+    # assert(value_map_score(b, 'black') == -11)
+    # # testing negative diagonal
+    # b.make_move((N,1), (3,2))
+    # assert(value_map_score(b, 'white') == 9)
+    # assert(value_map_score(b, 'black') == -9)
+    # assert(chess_like_score(b, 'white') == 0)
+    # assert(chess_like_score(b, 'black') == 0)
+    # # testing moving (white, 4) from a diagonal into a nondiagonal
+    # b.make_move((1,1), (1,3))
+    # assert(value_map_score(b, 'white') == 5)
+    # assert(chess_like_score(b, 'white') == 0)
+    # # testing 2-piece stack
+    # b.make_move((1,2), (2,2))
+    # assert(value_map_score(b, 'white') == 1)
+    # assert(chess_like_score(b, 'white') == 0)
+    # # R = Renderer(512)
+    # # R.draw_board(b)
